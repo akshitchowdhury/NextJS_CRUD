1b9cab3968024fd81420b4c5312e4ed804a80e45
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-04-16 19:52:50 +0530

    crudDump2

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -32,10 +32,10 @@ const TopicsList =async () => {
   return (
     <>
         {
-            topics.map(t=> (
+            topics.map((t,index)=> (
                     
-        <div className='p-4 border border-slate-300 my-3
-        flex justify-between gap-5 items-start'>
+        <div className='p-4 border border-slate-300 my-3 mx-32
+        flex justify-between gap-5 items-start' key={index}>
             <div>
                 <h2 className='font-bold text-2xl'>{t.title}</h2>
                 <div>{t.description}</div>
