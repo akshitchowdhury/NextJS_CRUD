e28c24832bdc7a7f19e18b090bb9c552eaad8ed7
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-04-24 11:25:17 +0530

    envDone6

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectMongoDb = async ()=>{
     try {
-        
+        console.log(process.env.MONGO_URI)
        await mongoose.connect(`${process.env.MONGO_URI}`,
        { useNewUrlParser: true, useUnifiedTopology: true })
         console.log("connected to Mongo DB")
